8d8ee86c45a099a393351b4394e8e0e2f0c45363
dodiman <dodimantakimpoo@gmail.com>
2024-09-26 06:42:11 +0800

    menambahkan pengaturan untuk deployment vercel

--- si_akademik/wsgi.py
+++ si_akademik/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'si_akademik.settings')
 
 application = get_wsgi_application()
+
+# untuk vercel
+app = application
